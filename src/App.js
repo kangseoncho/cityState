6618d490e967a6e481e73872d915eff6be35c32b
Kangseon Cho <kangseon.cho@gmail.com>
2017-08-10 17:22:05 -0700

    change App as a controller for connecting API call and view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import getZipCodeData from './getZipCodeData.js';
+import View from './view.jsx';
 
 /**
  * Make an App that has an
@@ -22,107 +24,93 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipCodeList: [],
-      zipCodeInput: undefined,
+      zipCodeList: [
+        { 90701: "Artesia, CA", "selected": 'none' }
+      ],
+      zipCodeInput: undefined
     };
 
-    this.saveLocation = this.saveLocation.bind(this);
-    this.inputZipCode = this.inputZipCode.bind(this);
-    this.toggleDisplay = this.toggleDisplay.bind(this);
-    this.fillInputWithZipcode = this.fillInputWithZipcode.bind(this);
-    this.updateZipCodeList = this.updateZipCodeList.bind(this);
+    this.addZipCodeToList = this.addZipCodeToList.bind(this);
+    this.changeDataForZipCodeList = this.changeDataForZipCodeList.bind(this);
+    this.checkDuplicateZipCode = this.checkDuplicateZipCode.bind(this);
+    this.updateZipCodeInput = this.updateZipCodeInput.bind(this);
+    this.highlightCityState = this.highlightCityState.bind(this);
   }
 
-  componentDidMount() {
-    //load up initial item for the list
-    fetch('https://api.zippopotam.us/us/90210')
-      .then(res => res.json())
-      .then(cityData => {
-        this.updateZipCodeList(cityData);
+  updateZipCodeInput(event) {
+    this.setState({zipCodeInput: event.target.value});
+  }
+
+  //modify the data retrieved from API call into what will be in state
+  changeDataForZipCodeList(data) {
+    let zipCode = {}
+    let cityState = `${data.places[0]['place name']}, ${data.places[0]['state abbreviation']}`;
+    zipCode[data['post code']] = cityState;
+    zipCode['selected'] = 'selected';
+
+    this.highlightCityState(data['post code']);
+
+    return zipCode;
+  }
+
+  //check if the zip code input already exists
+  checkDuplicateZipCode(changedData) {
+    let doesZipCodeExist = false;
+
+    this.state.zipCodeList.map((zipCodeItem) => {
+      if (Object.keys(zipCodeItem)[0] === Object.keys(changedData)[0]) {
+        doesZipCodeExist = true;
+      }
+      return zipCodeItem;
+    })
+
+    if(doesZipCodeExist === false) return changedData;
+    if(doesZipCodeExist === true) return undefined;
+  }
+
+  //add the newly created object for zipCodeList into the zipCodeList state
+  addZipCodeToList(zipCode) {
+    getZipCodeData(zipCode)
+      .then(res => {
+        return this.changeDataForZipCodeList(res);
+      })
+      .then(changedData => {
+        const tempZipCodeList = this.state.zipCodeList;
+        const filteredZipCode = this.checkDuplicateZipCode(changedData);
+
+        if(filteredZipCode !== undefined) tempZipCodeList.push(filteredZipCode);
+
+        this.setState({zipCodeList: tempZipCodeList});
       })
   }
 
-  //update list when new item is added
-  updateZipCodeList(cityData) {
+  //highlight city, state on click
+  highlightCityState(zipCode) {
     let tempZipCodeList = this.state.zipCodeList;
-    let newCityInfo = {};
-    //object with zip, city, and state. Also will house class for indicating selection
-    newCityInfo[cityData['post code']] = `${cityData.places[0]['place name']}, ${cityData.places[0]['state abbreviation']}`
-    newCityInfo['selected'] = 'notSelected';
-    tempZipCodeList.push(newCityInfo);
-    this.setState({ zipCodeList: tempZipCodeList });
-  }
 
-  //bold the selected city, state
-  toggleDisplay(cityInfo) {
-    const tempZipCodeList = this.state.zipCodeList.map((cities, index) => {
-
-      if (Object.keys(cities)[0] === Object.keys(cityInfo)[0]) {
-        if (cities['selected'] === 'selected') cities['selected'] = 'notSelected';
-        else cities['selected'] = 'selected';
+    tempZipCodeList = tempZipCodeList.map((zipCodeItem, index) => {
+      if(Object.keys(zipCodeItem)[0] !== zipCode) {
+        zipCodeItem[Object.keys(zipCodeItem)[1]] = 'none';
+      } else if(Object.keys(zipCodeItem)[0] === zipCode) {
+        zipCodeItem[Object.keys(zipCodeItem)[1]] = 'selected';
       }
-      else {
-        cities['selected'] = 'notSelected';
-      }
-
-      return cities;
+      return zipCodeItem;
     })
 
     this.setState({ zipCodeList: tempZipCodeList })
   }
 
-  //remember zipcode input
-  inputZipCode(e) {
-    this.setState({ zipCodeInput: e.target.value });
-  }
-
-  //enter new location to the list of city, state if one doesn't already exist
-  saveLocation() {
-
-    fetch(`https://api.zippopotam.us/us/${this.state.zipCodeInput}`)
-      .then(res => res.json())
-      .then(cityData => {
-        let alreadyInList = false;
-        //go through the list and see if same entry already exists
-        this.state.zipCodeList.map((cities) => {
-          if (Object.keys(cities)[0] === cityData['post code']) alreadyInList = true;
-        })
-        //since no existing entry found, add to list
-        if (alreadyInList === false) {
-          this.updateZipCodeList(cityData);
-          this.toggleDisplay(cityData);
-        }
-      })
-  }
-
-  //input field's value becomes the zipcode from city & state
-  fillInputWithZipcode(cityInfo) {
-    this.setState({ zipCodeInput: Object.keys(cityInfo)[0] });
-  }
-
   render() {
 
-    const displayCityState = this.state.zipCodeList.map((cityInfo, index) => {
-      return (
-        <div key={index} onClick={() => { this.toggleDisplay(cityInfo); this.fillInputWithZipcode(cityInfo) }}
-          className={cityInfo['selected'] + ' ' + 'location'}>
-          {cityInfo[Object.keys(cityInfo)[0]]}
-        </div>
-      )
-    })
-
     return (
-      <div className="App">
-        <h2>Find Cities by Zipcode</h2>
-
-        <div id="location">
-          {displayCityState}
-        </div>
-
-        <div id="formDiv">
-          <input type="text" value={this.state.zipCodeInput} placeholder="Zip Code" onChange={this.inputZipCode}></input>
-          <button type='submit' onClick={() => this.saveLocation()}> enter zipcode </button>
-        </div>
+      <div>
+        <View
+          zipCodeList={this.state.zipCodeList}
+          addZipCodeToList={this.addZipCodeToList}
+          zipCodeInput={this.state.zipCodeInput}
+          updateZipCodeInput={this.updateZipCodeInput}
+          highlightCityState={this.highlightCityState}
+        />
       </div>
     );
   }
